85702434c8aed41eab989941ae215f5aec1bc963
Arbaz yousaf <arbaz.k480@yahoo.com>
2024-08-18 14:30:16 -0700

    " Made a working qr code function that takes cart items and show it in whatsapp chat "

--- js/checkout.js
+++ js/checkout.js
@@ -3,7 +3,7 @@ const showSummary = (elem) => {
     (currentStatus === "false") ? elem.setAttribute("aria-expanded", "true") : elem.setAttribute("aria-expanded", "false")
 }
 const cartItems = JSON.parse(sessionStorage.getItem("cart"));
-
+const phoneNumber = '03187199492';
 // Show cart items
 function appendCartItemsInCheckoutPage() {
     const orderSummary = document.querySelector(".order-summary .cart-items");
@@ -40,6 +40,48 @@ appendCartItemsInCheckoutPage();
 // collecting data from cart and putting it in form
 let order = document.getElementById("order");
 
+if (cartItems != null && cartItems != undefined) {
+
 cartItems.forEach(item => {
     order.innerHTML += JSON.stringify(item, null, 2);
 })
+}
+
+// Make qr code
+function generateQrCode(qrContent) {
+    return new QRCode(document.getElementById("qr-code"), {
+      text: qrContent,
+      width: 256,
+      height: 256,
+      colorDark: "#000000",
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+}
+
+function encodeTextToUrl(text) {
+    return encodeURI(text);
+}
+function filterCart(items) {
+    let arr = [];
+    items.map(item => {
+        arr.push(
+            {
+                id: item.id,
+                name: item.name,
+                size: item.size,
+                color: item.color,
+                price: item.price
+            }
+        )
+    })
+    return arr;
+}
+const qrcodeLink = document.querySelector(".qr-code a");
+if (cartItems == null || cartItems == undefined || cartItems == "" || cartItems == []) {
+    generateQrCode(`https://wa.me/${phoneNumber}/?text=Hi`);
+    qrcodeLink.href = `https://wa.me/${phoneNumber}/?text=Hi`;
+} else {
+    qrcodeLink.href = `https://wa.me/${phoneNumber}/?text=${encodeTextToUrl(JSON.stringify(filterCart(cartItems)))}`;
+    generateQrCode(`https://wa.me/${phoneNumber}/?text=${encodeTextToUrl(JSON.stringify(filterCart(cartItems)))}`)
+}
